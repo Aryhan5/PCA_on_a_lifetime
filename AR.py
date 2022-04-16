e82dc6d450dc4e8ab9c6795b79bec301ccd893af
Aryhan5 <serena.sadek@outlook.com>
2022-04-16 22:03:56 +0200

    Apriltag detection is kinda working

--- AR.py
+++ AR.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import imageio
+from pupil_apriltags import Detector
 
 from pathlib import Path
 import sys
@@ -15,7 +15,7 @@ import sys
 import moms_apriltag as apt
 
 def generate(rng, scale=7): #Scale up 7x for an aprox 1.5cm tag
-    tags = apt.generate("tag36h10", range(1, rng+1))
+    tags = apt.generate("tag36h11", range(1, rng+1)) #range below 587 
     for tag in tags:
         img = 255*tag.array
         
@@ -28,7 +28,10 @@ def generate(rng, scale=7): #Scale up 7x for an aprox 1.5cm tag
         cv2.imwrite(name, scaled)
 
 def detect(picture):
-    print()
+    apt_detector = Detector(families='tag36h11')
+    tags = apt_detector.detect(picture)
+    return tags
+
 
 def identify():
     print()
@@ -36,3 +39,7 @@ def identify():
 def scale(map1, map2):
     print()
 
+img = cv2.imread('../stockapril.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+tags = detect(gray)
+print(tags)
